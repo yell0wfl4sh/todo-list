2e57046dad97a55a47aed81444df2420e2cc3adf
Yash Agrawal <yagrawal900@gmail.com>
2019-02-10 00:46:42 +0530

    GUI: Add update feature

--- gui.py
+++ gui.py
@@ -6,7 +6,7 @@ from gi.repository import Gtk
 from gi.repository import Gdk
 from gi.repository import Gio
 import main
-from main import fetch_entries, add_entry
+from main import fetch_entries, add_entry, fetch_entry, update_entry
 import sqlite3
 
 class CalDialog(Gtk.Dialog):
@@ -33,6 +33,69 @@ class CalDialog(Gtk.Dialog):
     def cal_entry(self, calendar, year, month, date):
         self.value = calendar.get_date()
 
+class UpdateDialog(Gtk.Dialog):
+
+    def __init__(self, parent, id):
+        Gtk.Dialog.__init__(self, "Update Value", parent, 0,
+            (Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL,
+             Gtk.STOCK_OK, Gtk.ResponseType.OK))
+
+        self.set_default_size(300, 200)
+        note = fetch_entry(id)
+        hbox2 = self.get_content_area()
+
+        entry1 = Gtk.Entry()
+        entry1.set_text(note[1])
+        hbox2.pack_start(entry1, True, True, 0)
+
+        entry2 = Gtk.Entry()
+        entry2.set_text(note[2])
+        hbox2.pack_start(entry2, True, True, 0)
+
+        hbox = Gtk.Box(spacing=10, orientation=Gtk.Orientation.HORIZONTAL)
+        hbox.set_homogeneous(True)
+
+        vbox2 = Gtk.Box(spacing=10, orientation=Gtk.Orientation.VERTICAL)
+
+        cal_button = Gtk.Button()
+        icon_cal = Gio.ThemedIcon(name="gnome-calendar")
+        image_cal = Gtk.Image.new_from_gicon(icon_cal, Gtk.IconSize.BUTTON)
+        cal_button.add(image_cal)
+        cal_button.set_tooltip_text("Pick date")
+        cal_button.connect("clicked", self.on_cal_clicked)
+        vbox2.pack_start(cal_button, True, True, 0)
+        hbox.add(vbox2)
+
+        vbox2 = Gtk.Box(spacing=10, orientation=Gtk.Orientation.VERTICAL)
+        checkbut = Gtk.CheckButton("Status")
+        if(note[4]):
+            checkbut.set_active(True)
+        checkbut.connect("toggled", self.on_checked, checkbut)
+        vbox2.pack_start(checkbut, True, True, 0)
+
+        hbox.add(vbox2)
+        hbox2.add(hbox)
+
+        self.title = entry1
+        self.comment = entry2
+        self.dateval = note[3]
+        self.status = note[4]
+        self.show_all()
+
+    def on_checked(self, widget, checkbut):
+        if checkbut.get_active():
+            self.status = 1
+        else:
+            self.status = 2
+
+    def on_cal_clicked(self, widget):
+        """ Open calender and get user selection """
+        dialog = CalDialog(self)
+        response = dialog.run()
+        if response == Gtk.ResponseType.OK:
+            self.dateval = str(dialog.value)
+        dialog.destroy()
+
 class ToDoApp(Gtk.Window):
     """
     Main Class
@@ -43,7 +106,7 @@ class ToDoApp(Gtk.Window):
         self.set_title('To Do App')
         self.set_default_size(600, 900)
         self.set_border_width(10)
-        self.dateval = "(year=2019, month=11, day=19)"
+        self.dateval = "(year=2019, month=02, day=10)"
         mainbox = Gtk.Box(spacing=10, orientation=Gtk.Orientation.VERTICAL)
 
         hbox1 = Gtk.Box(spacing=10, orientation=Gtk.Orientation.HORIZONTAL)
@@ -97,7 +160,7 @@ class ToDoApp(Gtk.Window):
                 hbox.pack_start(label1, True, True, 0)
 
             updatebutton = Gtk.Button.new_with_label("Update")
-            updatebutton.connect("clicked", self.update_note)
+            updatebutton.connect("clicked", self.update_note, note[0])
             hbox.pack_start(updatebutton, True, True, 0)
 
             listbox.add(row)
@@ -123,7 +186,7 @@ class ToDoApp(Gtk.Window):
                 hbox.pack_start(label1, True, True, 0)
 
             updatebutton = Gtk.Button.new_with_label("Update")
-            updatebutton.connect("clicked", self.update_note)
+            updatebutton.connect("clicked", self.update_note, note[0])
             hbox.pack_start(updatebutton, True, True, 0)
 
             listbox.add(row)
@@ -140,27 +203,37 @@ class ToDoApp(Gtk.Window):
 
     def add_note(self, widget, title, comment):
         """ Add new note """
-        datetime = self.parse_date()
+        datetime = self.parse_date(self.dateval)
         add_entry(title.props.text, comment.props.text, datetime)
         self.update_display()
 
-    def update_note(self, widget):
+    def update_note(self, widget, id):
         """ Update existing note """
-        pass
+        dialog = UpdateDialog(self, id)
+        response = dialog.run()
+        if response == Gtk.ResponseType.OK:
+            datetime = self.parse_date(dialog.dateval)
+            update_entry(id, dialog.title.props.text, dialog.comment.props.text, datetime, dialog.status)
+            self.update_display()
+        dialog.destroy()
 
-    def parse_date(self):
-        vals = (self.dateval).split(',')
-        datetime = vals[0][-4:]
-        if(len(vals[1])==8):
-            datetime = datetime + "-0" + vals[1][-1:]
-        else:
-            datetime = datetime + "-" +  vals[1][-2:]
 
-        if(len(vals[2])==7):
-            datetime = datetime + "-0" + vals[2][-2:-1]
+    def parse_date(self, dateval):
+        if len(dateval)>10:
+            vals = (dateval).split(',')
+            datetime = vals[0][-4:]
+            if(len(vals[1])==8):
+                datetime = datetime + "-0" + vals[1][-1:]
+            else:
+                datetime = datetime + "-" +  vals[1][-2:]
+
+            if(len(vals[2])==7):
+                datetime = datetime + "-0" + vals[2][-2:-1]
+            else:
+                datetime = datetime + "-" + vals[2][-3:-1]
+            return datetime
         else:
-            datetime = datetime + "-" + vals[2][-3:-1]
-        return datetime
+            return dateval
 
 win = ToDoApp()
 win.connect("destroy", Gtk.main_quit)
